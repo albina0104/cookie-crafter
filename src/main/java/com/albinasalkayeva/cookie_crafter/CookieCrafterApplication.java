92b189196195208c296c8c7f143934ebe0102276
Albina Salkayeva <salkaeva.a@gmail.com>
2025-02-01 01:11:38 +0500

    Initialize project

---
+++ src/main/java/com/albinasalkayeva/cookie_crafter/CookieCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.albinasalkayeva.cookie_crafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieCrafterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albinasalkayeva/cookie_crafter/CookieCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albinasalkayeva.cookie_crafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
