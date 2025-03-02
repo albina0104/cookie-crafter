6bec2d53ab57e002e2218bfc2ee8caa7975afe36
Albina Salkayeva <salkaeva.a@gmail.com>
2025-03-02 17:57:13 +0500

    Add content to About modal

--- cookie-crafter-frontend/src/components/Footer.jsx
+++ cookie-crafter-frontend/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './footer.css';
 import { Modal } from './Modal';
+import logo from '../assets/cookie-crafter-logo-transparent-2.webp';
 
 export function Footer() {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,10 +23,47 @@ export function Footer() {
           About
         </button>
         <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          repudiandae ipsam excepturi nisi voluptas esse ab quos error id
-          debitis maxime fugiat optio officiis sed mollitia, illum reiciendis
-          ipsum dolore.
+          <h2>About</h2>
+          <div className='about-modal-content'>
+            <p>
+              <img
+                src={logo}
+                alt='Cookie Crafter'
+                className='cookie-crafter-logo-big'
+              />
+            </p>
+            <div className='about-modal-content__text'>
+              <ul>
+                <li>Created by Albina Salkayeva</li>
+                <li>Logo generated with ChatGPT (DALL·E)</li>
+                <li>
+                  Icons sourced from{' '}
+                  <a
+                    href='https://www.svgrepo.com'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    SVG Repo
+                  </a>
+                </li>
+                <li>
+                  Source code available on{' '}
+                  <a
+                    href='https://github.com/albina0104/cookie-crafter'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+              <p>
+                If you have any questions or feedback about the project, feel
+                free to reach out via email at{' '}
+                <a href='mailto:salkaeva.a@gmail.com'>salkaeva.a@gmail.com</a>!
+              </p>
+            </div>
+          </div>
         </Modal>
       </section>
     </footer>
